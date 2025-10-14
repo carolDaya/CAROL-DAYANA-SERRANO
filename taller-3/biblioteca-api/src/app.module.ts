c658cfa1868b9dabcc57d69b494ba06b363921be
YurleyG7 <gualdronyurley7@gmail.com>
2025-10-13 20:33:59 -0600

    Creacion de Modulos

--- taller-3/biblioteca-api/src/app.module.ts
+++ taller-3/biblioteca-api/src/app.module.ts
@@ -1,9 +1,43 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { BooksModule } from './modules/books/books.module';
+import { AuthorsModule } from './modules/authors/authors.module';
+import { CategoriesModule } from './modules/categories/categories.module';
+import { LoansModule } from './modules/loans/loans.module';
+import { ReviewsModule } from './modules/reviews/reviews.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { BooksModule } from './books/books.module';
+import { AuthorsModule } from './authors/authors.module';
+import { CategoriesModule } from './categories/categories.module';
+import { LoansModule } from './loans/loans.module';
+import { ReviewsModule } from './reviews/reviews.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    UsersModule,
+    AuthModule,
+    BooksModule,
+    AuthorsModule,
+    CategoriesModule,
+    LoansModule,
+    ReviewsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
